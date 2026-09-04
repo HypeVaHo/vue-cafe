7f9525c4b7ff15e70825b91329ff25b2a349b509
HypeVaHo <124986792+HypeVaHo@users.noreply.github.com>
2026-09-04 07:51:18 +0300

    Update app-config.js

--- public/app-config.js
+++ public/app-config.js
@@ -2,5 +2,5 @@
 // Для сайта на GitHub Pages: укажите здесь адрес бэкенда (туннель/сервер),
 // например: window.APP_CONFIG = { apiUrl: 'https://ваш-туннель.trycloudflare.com/api' };
 window.APP_CONFIG = {
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://vue-cafe-api.loca.lt'
 };
