f159905123810c3886d2b392aee74185536ab18c
HypeVaHo <kxss8531@gmail.com>
2026-09-09 06:04:18 +0300

    Switch to stable Serveo tunnel URL

--- public/app-config.js
+++ public/app-config.js
@@ -1,6 +1,6 @@
 ﻿// онфигурация приложения (переопределяется при деплое).
 // ля сайта на GitHub Pages: укажите здесь адрес бэкенда (туннель/сервер).
-// LocalTunnel: https://vue-cafe-api.loca.lt (постоянный URL)
+// Serveo: https://vuecafe-api.serveousercontent.com (стабильный URL)
 window.APP_CONFIG = {
-  apiUrl: 'https://vue-cafe-api.loca.lt/api'
+  apiUrl: 'https://vuecafe-api.serveousercontent.com/api'
 };
