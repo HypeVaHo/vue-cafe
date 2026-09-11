dca12cdce4b550127db29972920eb2740160ab66
HypeVaHo <kxss8531@gmail.com>
2026-09-11 13:52:30 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://passive-basename-beats-soma.trycloudflare.com/api'
+  apiUrl: 'https://tradition-amounts-treated-uri.trycloudflare.com/api'
 };
