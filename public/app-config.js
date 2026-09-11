cde8036fb8210e61a37dc7eda35aa7dba3aa9a3c
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:08:00 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hugo-taxes-driving-ver.trycloudflare.com/api'
+  apiUrl: 'https://address-better-gui-towns.trycloudflare.com/api'
 };
