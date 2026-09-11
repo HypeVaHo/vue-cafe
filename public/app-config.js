b876e9aea55c8c102893a4ca40257aef0340633a
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:48:15 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://fairy-civilian-manga-greatest.trycloudflare.com/api'
+  apiUrl: 'https://awareness-allan-antarctica-eur.trycloudflare.com/api'
 };
