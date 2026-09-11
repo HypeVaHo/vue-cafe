8f8c62f25f9393151590ba6c72f1fecff07cbf0c
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:21:16 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://total-temporarily-calculations-willow.trycloudflare.com/api'
+  apiUrl: 'https://social-rats-nail.loca.lt/api'
 };
