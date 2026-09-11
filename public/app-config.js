60d0683cb838535b536a6974b732eebbf8960436
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:31:33 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://note-warren-hardwood-behind.trycloudflare.com/api'
+  apiUrl: 'https://disclaimer-relax-becomes-univ.trycloudflare.com/api'
 };
