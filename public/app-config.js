09d72bd3c3ef074e30a4379ca6cd2c8c1460c26b
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:47:46 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://media-salt-educated-elephant.trycloudflare.com/api'
+  apiUrl: 'https://literally-george-authentication-tired.trycloudflare.com/api'
 };
