fccd54dbd8b6ff8727b46a2c1bc7382d0bee47be
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:52:50 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://headset-telephone-realm-globe.trycloudflare.com/api'
+  apiUrl: 'https://consortium-firewall-violation-endangered.trycloudflare.com/api'
 };
