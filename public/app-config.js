cbbcbe54999a69b33e10c81ca5841feeeb040023
HypeVaHo <kxss8531@gmail.com>
2026-09-11 13:56:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://idol-remix-align-bonds.trycloudflare.com/api'
+  apiUrl: 'https://ultra-polls-gibraltar-robot.trycloudflare.com/api'
 };
