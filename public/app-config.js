1537f038eb607139206dcc688703a86db6a5306d
HypeVaHo <kxss8531@gmail.com>
2026-09-11 11:46:55 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://smart-roses-wait.loca.lt/api'
+  apiUrl: 'https://passive-basename-beats-soma.trycloudflare.com/api'
 };
