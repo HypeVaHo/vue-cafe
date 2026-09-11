d86493efd0aa56a8769b658042845164fc940bf1
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:02:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://producing-prayers-bride-bride.trycloudflare.com/api'
+  apiUrl: 'https://cemetery-organizational-homeless-automatic.trycloudflare.com/api'
 };
