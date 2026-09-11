c2ed15ed73f237f2f87841cf5ac8315884b03796
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:13:34 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://perhaps-shadows-totals-diana.trycloudflare.com/api'
+  apiUrl: 'https://iii-desktops-similarly-hired.trycloudflare.com/api'
 };
