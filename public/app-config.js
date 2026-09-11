571bd436e1342e6764ac8297dab73b1b12917159
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:44:39 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://cap-arctic-dialogue-dramatically.trycloudflare.com/api'
+  apiUrl: 'https://next-cleveland-nearly-dawn.trycloudflare.com/api'
 };
