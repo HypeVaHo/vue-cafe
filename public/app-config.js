6e2ee60aeeff6143639f36b337e691741295bd48
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:21:50 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://cannon-supporting-pig-lamps.trycloudflare.com/api'
+  apiUrl: 'https://etc-seem-written-dish.trycloudflare.com/api'
 };
