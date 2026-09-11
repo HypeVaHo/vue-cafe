1cd644f7694e4b56fcd926ef9a70bbbe5690a8a2
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:04:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://flux-allowance-korea-rev.trycloudflare.com/api'
+  apiUrl: 'https://filing-cod-specializing-belongs.trycloudflare.com/api'
 };
