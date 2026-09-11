f06c3bf614a1afb30b3078eea9c76fd7d56348fd
HypeVaHo <kxss8531@gmail.com>
2026-09-11 22:05:55 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://attachments-cook-trying-auction.trycloudflare.com/api'
+  apiUrl: 'https://stretch-fan-artist-tech.trycloudflare.com/api'
 };
