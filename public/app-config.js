001f5fb8586ae71f208ccce9b23041d92491e238
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:43:11 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://towers-penguin-senior-hazardous.trycloudflare.com/api'
+  apiUrl: 'https://perry-anaheim-wit-citysearch.trycloudflare.com/api'
 };
