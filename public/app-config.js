94292ca85f0bf461849afe9b6f4b992c6ac038f0
HypeVaHo <kxss8531@gmail.com>
2026-09-11 22:10:08 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://systematic-showers-delegation-competitors.trycloudflare.com/api'
+  apiUrl: 'https://salty-mirrors-ring.loca.lt/api'
 };
