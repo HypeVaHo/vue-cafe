1c0ffdeafc0bac71cb265f9b0192283290d43991
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:50:39 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://awareness-allan-antarctica-eur.trycloudflare.com/api'
+  apiUrl: 'https://combo-papers-efficient-analyses.trycloudflare.com/api'
 };
