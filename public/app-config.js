1dd06d29e3a21d4ad5147d9f8baa428a6e07f8ca
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:10:58 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://suppose-lol-pie-substitute.trycloudflare.com/api'
+  apiUrl: 'https://thru-phi-parts-ericsson.trycloudflare.com/api'
 };
