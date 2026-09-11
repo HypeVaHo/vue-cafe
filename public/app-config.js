2b3a888646cea60ea917a81d27103b8a18085e59
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:50:24 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://crops-sticker-drug-aviation.trycloudflare.com/api'
+  apiUrl: 'https://headset-telephone-realm-globe.trycloudflare.com/api'
 };
