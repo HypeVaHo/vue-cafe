0497fab6061dacd901f4b5c91fc49c8f38370fce
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:55:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://dominant-differ-orders-argue.trycloudflare.com/api'
+  apiUrl: 'https://but-click-ensures-ssl.trycloudflare.com/api'
 };
