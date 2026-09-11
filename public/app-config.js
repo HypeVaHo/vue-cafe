f6b112baeaf23387aef899a374b1d9562d386dd5
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:03:12 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://european-acoustic-roots-throat.trycloudflare.com/api'
+  apiUrl: 'https://eminem-attraction-mouth-blond.trycloudflare.com/api'
 };
