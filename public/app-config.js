da7a8767a27f1ce40c380f30296c6019ce9132ed
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:56:53 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://but-click-ensures-ssl.trycloudflare.com/api'
+  apiUrl: 'https://aqua-venture-graduate-dramatic.trycloudflare.com/api'
 };
