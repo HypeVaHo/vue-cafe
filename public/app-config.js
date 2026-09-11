ad2274f93e1c07f2214bb4cc31d3ad55486b882d
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:16:56 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hands-fantastic-london-elect.trycloudflare.com/api'
+  apiUrl: 'https://himself-stuart-shadows-allan.trycloudflare.com/api'
 };
