8fc787cb9dafeedf3d0afabb1ea2b2e7099ebd25
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:16:02 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://politics-emily-polyester-recipient.trycloudflare.com/api'
+  apiUrl: 'https://mouse-invisible-carl-alike.trycloudflare.com/api'
 };
