bdab356db46591757905971b8638aa6bf3a23868
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:40:55 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://christina-unable-molecules-rays.trycloudflare.com/api'
+  apiUrl: 'https://entrepreneur-fourth-opposition-prot.trycloudflare.com/api'
 };
