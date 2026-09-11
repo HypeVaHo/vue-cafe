af6de6c150fa0f234183df3903286f5e86cfeef1
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:42:18 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hearings-assessments-beliefs-photography.trycloudflare.com/api'
+  apiUrl: 'https://cap-arctic-dialogue-dramatically.trycloudflare.com/api'
 };
