07fe8aa879ce2c311185b3e03dd5845e8f7416f4
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:14:17 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://travelling-safely-compressed-designated.trycloudflare.com/api'
+  apiUrl: 'https://translated-coral-overseas-spread.trycloudflare.com/api'
 };
