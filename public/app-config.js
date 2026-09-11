958df8ace8ba7bba986543e5176e23c619804bc7
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:03:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://cemetery-organizational-homeless-automatic.trycloudflare.com/api'
+  apiUrl: 'https://flux-allowance-korea-rev.trycloudflare.com/api'
 };
