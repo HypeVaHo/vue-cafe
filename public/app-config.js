7fdb1deeae931034320de24c8ada19346072a535
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:22:30 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://etc-seem-written-dish.trycloudflare.com/api'
+  apiUrl: 'https://phillips-warranty-online-quit.trycloudflare.com/api'
 };
