f70e8f074ff2b1784e19c16b2643d1dad6fe1521
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:40:43 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://field-injured-bull-wires.trycloudflare.com/api'
+  apiUrl: 'https://hearings-assessments-beliefs-photography.trycloudflare.com/api'
 };
