ea7a86457efad54a9dfe66f1eb30fe28409b59fa
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:00:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://stationery-doctrine-hawaii-reduce.trycloudflare.com/api'
+  apiUrl: 'https://producing-prayers-bride-bride.trycloudflare.com/api'
 };
