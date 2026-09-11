c4481c4d0dbf25f452a55486a07677baefb6a9ea
HypeVaHo <kxss8531@gmail.com>
2026-09-11 20:11:50 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://unlike-ted-screensaver-implies.trycloudflare.com/api'
+  apiUrl: 'https://charging-belong-fourth-apply.trycloudflare.com/api'
 };
