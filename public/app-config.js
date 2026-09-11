0ed4b8272a7ca92185494357518dd435e2558f63
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:23:48 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://priest-exercises-matt-expenses.trycloudflare.com/api'
+  apiUrl: 'https://auctions-print-fisher-researchers.trycloudflare.com/api'
 };
