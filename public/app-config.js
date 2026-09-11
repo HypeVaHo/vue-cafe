a673e330d6a16d8dba3f96638742a49b0d826bce
HypeVaHo <kxss8531@gmail.com>
2026-09-11 20:16:01 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://specification-heading-premises-asks.trycloudflare.com/api'
+  apiUrl: 'https://army-offerings-stable-manufactured.trycloudflare.com/api'
 };
