d061bfeb9864aeb09ed2a972d38a205e9f65daa0
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:39:37 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://repository-assured-foam-administrators.trycloudflare.com/api'
+  apiUrl: 'https://christina-unable-molecules-rays.trycloudflare.com/api'
 };
