b7c76568ab4f13fa1fedc99589504b08949d2820
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:09:19 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://address-better-gui-towns.trycloudflare.com/api'
+  apiUrl: 'https://montgomery-citations-ian-declared.trycloudflare.com/api'
 };
