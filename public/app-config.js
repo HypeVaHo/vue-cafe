8729c88299d3ddc4104718ad74a08db0b2b12538
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:23:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://experiences-microwave-racial-power.trycloudflare.com/api'
+  apiUrl: 'https://authentication-buffer-artists-port.trycloudflare.com/api'
 };
