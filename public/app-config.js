7d1fc0c790a4b8f7210f47ed2993a64500a0f55b
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:53:17 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://athletic-devices-peer-august.trycloudflare.com/api'
+  apiUrl: 'https://panel-poems-ears-halo.trycloudflare.com/api'
 };
