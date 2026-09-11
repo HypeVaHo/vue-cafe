4e2269e3cc8e94849ed9b60df89c3b98090e91fb
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:30:13 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://cindy-brian-cigarette-tonight.trycloudflare.com/api'
+  apiUrl: 'https://note-warren-hardwood-behind.trycloudflare.com/api'
 };
