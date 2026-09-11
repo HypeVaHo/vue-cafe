3ecd3490b317907072ef00d9bafe2a9e8b8b182f
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:58:10 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://equations-horizon-bracket-arcade.trycloudflare.com/api'
+  apiUrl: 'https://speech-easy-wars-harvard.trycloudflare.com/api'
 };
