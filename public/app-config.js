a50a3733424a1249a9977a114ddf250e690825d9
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:13:36 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://grill-maintaining-wax-spatial.trycloudflare.com/api'
+  apiUrl: 'https://supplies-gaps-alternatively-remote.trycloudflare.com/api'
 };
