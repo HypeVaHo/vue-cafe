e0fa9bd205a275a1b4d3a2af78f175323fd8bcfd
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:00:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://urgent-conventions-intervals-pills.trycloudflare.com/api'
+  apiUrl: 'https://already-epa-journals-ferry.trycloudflare.com/api'
 };
