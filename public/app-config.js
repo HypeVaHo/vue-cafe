db869662d2efaa1833bcc0f1880184825a453f17
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:56:35 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://efforts-coat-merger-struct.trycloudflare.com/api'
+  apiUrl: 'https://century-diameter-sandwich-magnitude.trycloudflare.com/api'
 };
