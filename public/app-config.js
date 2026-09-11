39bde1ee5cbdeef7afc63f8fbe1c4622ca4e2481
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:05:36 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://dictionary-remaining-kentucky-icons.trycloudflare.com/api'
+  apiUrl: 'https://hugo-taxes-driving-ver.trycloudflare.com/api'
 };
