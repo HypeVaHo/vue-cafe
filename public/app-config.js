9222f5329f4d685439bf03b62342d2269e5294ae
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:00:48 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://includes-neon-feet-zones.trycloudflare.com/api'
+  apiUrl: 'https://european-acoustic-roots-throat.trycloudflare.com/api'
 };
