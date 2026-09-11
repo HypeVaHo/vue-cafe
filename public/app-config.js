080348e4c4e7b506ac988f3fa18b56d43468bc08
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:30:31 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://phillips-warranty-online-quit.trycloudflare.com/api'
+  apiUrl: 'https://archives-casual-gmt-mom.trycloudflare.com/api'
 };
