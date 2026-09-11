cbb5a199df1199932ecdde2ca5877aa0c635d17d
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:16:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://mechanics-certain-manufacture-operated.trycloudflare.com/api'
+  apiUrl: 'https://bar-candidate-ethics-temperatures.trycloudflare.com/api'
 };
