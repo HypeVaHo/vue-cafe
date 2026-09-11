5e57e4e8ca74279c486d090bf00d295c4b791c3e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:59:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://speech-easy-wars-harvard.trycloudflare.com/api'
+  apiUrl: 'https://includes-neon-feet-zones.trycloudflare.com/api'
 };
