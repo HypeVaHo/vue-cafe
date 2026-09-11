7b0898c860595b026f504d59fcc445e71395f4ab
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:26:15 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://authentication-buffer-artists-port.trycloudflare.com/api'
+  apiUrl: 'https://dom-zoloft-cambridge-slope.trycloudflare.com/api'
 };
