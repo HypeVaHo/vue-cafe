3226540addffa302a8c996a8428f99fd6bb64222
HypeVaHo <kxss8531@gmail.com>
2026-09-11 20:10:26 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://bonus-jose-downtown-morning.trycloudflare.com/api'
+  apiUrl: 'https://unlike-ted-screensaver-implies.trycloudflare.com/api'
 };
