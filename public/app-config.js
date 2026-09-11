2d61761dd68ac583292de9147ad548c537344da8
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:38:07 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://times-trusts-digital-suppliers.trycloudflare.com/api'
+  apiUrl: 'https://hip-montreal-patch-generic.trycloudflare.com/api'
 };
