6a04c350cc4c516a849d0968901fa682387342f7
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:09:40 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://groundwater-handy-simply-rico.trycloudflare.com/api'
+  apiUrl: 'https://suppose-lol-pie-substitute.trycloudflare.com/api'
 };
