0b3b67394e10fc43add3e1446532d7a494234a34
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:10:07 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://manuals-sustainable-informative-figured.trycloudflare.com/api'
+  apiUrl: 'https://specialists-somehow-holland-shelf.trycloudflare.com/api'
 };
