2acf986d2e5218e267d91b4ac129888e523eca05
HypeVaHo <kxss8531@gmail.com>
2026-09-11 15:51:58 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://combo-papers-efficient-analyses.trycloudflare.com/api'
+  apiUrl: 'https://athletic-devices-peer-august.trycloudflare.com/api'
 };
