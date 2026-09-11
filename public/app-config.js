4690c2a3ac525f7a3d5519bb10dfdbb058525fd3
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:35:39 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://monroe-fur-flush-digest.trycloudflare.com/api'
+  apiUrl: 'https://organizational-aaa-accommodate-geographical.trycloudflare.com/api'
 };
