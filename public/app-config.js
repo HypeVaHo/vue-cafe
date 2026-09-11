c66dbf04a126d15f314f68822cb0479e93171696
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:15:26 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://gotten-related-steal-organisms.trycloudflare.com/api'
+  apiUrl: 'https://mechanics-certain-manufacture-operated.trycloudflare.com/api'
 };
