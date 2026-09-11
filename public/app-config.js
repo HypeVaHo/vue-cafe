6bf8c823931dad962a649fb8bac40c7ac474af19
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:59:30 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://iii-advertising-trance-mayor.trycloudflare.com/api'
+  apiUrl: 'https://stationery-doctrine-hawaii-reduce.trycloudflare.com/api'
 };
