09ad91ee3c5b9eeb3dd06655430b4a56cd83daa5
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:20:15 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://pee-server-fountain-speak.trycloudflare.com/api'
+  apiUrl: 'https://educated-raised-properly-gps.trycloudflare.com/api'
 };
