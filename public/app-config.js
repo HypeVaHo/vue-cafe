1ab118f1cf933994a2859e6ef43f919ee3fbac1e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:20:06 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://villages-configure-outlets-traditional.trycloudflare.com/api'
+  apiUrl: 'https://hat-moscow-lil-words.trycloudflare.com/api'
 };
