aaab057df146088ff453aed04ab6cc53b14a625e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:31:50 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://archives-casual-gmt-mom.trycloudflare.com/api'
+  apiUrl: 'https://height-expectations-sim-establishment.trycloudflare.com/api'
 };
