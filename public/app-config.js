a5cce3c72a6856d8c69f2f05e3fe9b10fde0e58b
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:21:24 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hat-moscow-lil-words.trycloudflare.com/api'
+  apiUrl: 'https://experiences-microwave-racial-power.trycloudflare.com/api'
 };
