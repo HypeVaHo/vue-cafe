ad04b3b11dca83a0c504ddc8400915cfaf9a8484
HypeVaHo <kxss8531@gmail.com>
2026-09-11 22:08:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://limit-montana-lips-advisory.trycloudflare.com/api'
+  apiUrl: 'https://systematic-showers-delegation-competitors.trycloudflare.com/api'
 };
