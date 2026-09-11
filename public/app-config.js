c32f82f65f92a7888a4e5a04c721c84cddb5d249
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:55:08 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://classical-effectively-property-premises.trycloudflare.com/api'
+  apiUrl: 'https://efforts-coat-merger-struct.trycloudflare.com/api'
 };
