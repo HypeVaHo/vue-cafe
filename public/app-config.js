8cc7ce158056ed0b330dea8555dfb3ea28e84658
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:24:48 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://love-webcast-democracy-advice.trycloudflare.com/api'
+  apiUrl: 'https://references-certified-alabama-calculate.trycloudflare.com/api'
 };
