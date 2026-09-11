2d298a29690fa1c5884476c6d97a3c8c7282e09e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:21:42 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://educated-raised-properly-gps.trycloudflare.com/api'
+  apiUrl: 'https://varies-residential-day-leaders.trycloudflare.com/api'
 };
