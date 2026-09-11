c47304d1a6c0d0b1c97ff08b1d657d9ac237ce81
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:35:52 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://gnu-julie-pharmacy-made.trycloudflare.com/api'
+  apiUrl: 'https://times-trusts-digital-suppliers.trycloudflare.com/api'
 };
