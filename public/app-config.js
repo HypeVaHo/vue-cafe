f988e76bc95de81e0e496e59a28627f61f2ef655
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:33:10 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://height-expectations-sim-establishment.trycloudflare.com/api'
+  apiUrl: 'https://brook-playing-smart-legendary.trycloudflare.com/api'
 };
