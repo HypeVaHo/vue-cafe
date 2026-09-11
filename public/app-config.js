d466e40b3af2a3987e3e774d3b0070b31d7ce024
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:47:52 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://villas-schema-mechanisms-syracuse.trycloudflare.com/api'
+  apiUrl: 'https://prep-aluminum-mountains-elder.trycloudflare.com/api'
 };
