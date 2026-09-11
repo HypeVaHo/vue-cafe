07179d94f05749e5b035e04f7f97afb5bf07714c
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:46:17 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://atomic-searches-reproduced-dolls.trycloudflare.com/api'
+  apiUrl: 'https://media-salt-educated-elephant.trycloudflare.com/api'
 };
