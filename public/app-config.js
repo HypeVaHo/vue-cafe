372c9315a8c49d7f0c3097bcc19a8354d57e9528
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:20:30 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://wants-carey-specifies-viii.trycloudflare.com/api'
+  apiUrl: 'https://cannon-supporting-pig-lamps.trycloudflare.com/api'
 };
