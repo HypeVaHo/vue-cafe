c60435413af2ab157aad2ecd2c2ad5be9db8117b
HypeVaHo <kxss8531@gmail.com>
2026-09-11 20:09:05 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://bow-broadway-crest-colleague.trycloudflare.com/api'
+  apiUrl: 'https://bonus-jose-downtown-morning.trycloudflare.com/api'
 };
