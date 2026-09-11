515e8e95391da3f8a5dfd6adc7315cc8a75ef3bc
HypeVaHo <kxss8531@gmail.com>
2026-09-11 22:07:23 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://stretch-fan-artist-tech.trycloudflare.com/api'
+  apiUrl: 'https://limit-montana-lips-advisory.trycloudflare.com/api'
 };
