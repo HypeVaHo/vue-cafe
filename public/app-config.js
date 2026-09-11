b61696840447eccba98b673ec5c26f5893941916
HypeVaHo <kxss8531@gmail.com>
2026-09-11 16:27:34 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://dom-zoloft-cambridge-slope.trycloudflare.com/api'
+  apiUrl: 'https://bloomberg-cosmetics-filing-greeting.trycloudflare.com/api'
 };
