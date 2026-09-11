54d3ad621b57fbc1776b7223185870517c5d472e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 22:02:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://adam-tuesday-written-inserted.trycloudflare.com/api'
+  apiUrl: 'https://pvc-activities-received-regulatory.trycloudflare.com/api'
 };
