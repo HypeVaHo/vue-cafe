fe91083f3d2fba0dd4384decc035d58d19c2f696
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:12:54 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://situated-railroad-summer-indicate.trycloudflare.com/api'
+  apiUrl: 'https://travelling-safely-compressed-designated.trycloudflare.com/api'
 };
