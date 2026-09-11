61b275ed7bbeab0b40efb7d946383524c8edf3f0
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:12:19 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://expertise-rankings-harley-nuclear.trycloudflare.com/api'
+  apiUrl: 'https://perhaps-shadows-totals-diana.trycloudflare.com/api'
 };
