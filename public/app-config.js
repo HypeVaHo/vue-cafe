62fd7a5da8c8132d877df9fad931ddfe48455771
HypeVaHo <kxss8531@gmail.com>
2026-09-11 20:07:38 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://clear-lamps-end.loca.lt/api'
+  apiUrl: 'https://bow-broadway-crest-colleague.trycloudflare.com/api'
 };
