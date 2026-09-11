ff6ccd7bda9c0cb7eae83a8c39f0a2923b91f01e
HypeVaHo <kxss8531@gmail.com>
2026-09-11 18:08:47 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hospitality-hitting-optimization-hitting.trycloudflare.com/api'
+  apiUrl: 'https://manuals-sustainable-informative-figured.trycloudflare.com/api'
 };
