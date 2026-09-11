4b51bfdc1df732f0504ce8315c63b2ade327ba2b
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:22:25 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://spots-grades-syndrome-pride.trycloudflare.com/api'
+  apiUrl: 'https://priest-exercises-matt-expenses.trycloudflare.com/api'
 };
