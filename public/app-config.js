272bc1d618ec7bd48ba8b221f7a16657b3d8b8e9
HypeVaHo <kxss8531@gmail.com>
2026-09-11 17:42:20 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://entrepreneur-fourth-opposition-prot.trycloudflare.com/api'
+  apiUrl: 'https://alleged-bucks-silence-architectural.trycloudflare.com/api'
 };
