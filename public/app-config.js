b33850dd8fcdfb391dd5bd16fa9bda3406e6c9c5
HypeVaHo <kxss8531@gmail.com>
2026-09-11 21:32:39 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://motorcycles-amount-astrology-names.trycloudflare.com/api'
+  apiUrl: 'https://hunt-humanities-task-surfing.trycloudflare.com/api'
 };
