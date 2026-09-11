d2b8ec24b5768ac7ac65d86d90031dd217653c66
HypeVaHo <kxss8531@gmail.com>
2026-09-11 23:14:47 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://iii-desktops-similarly-hired.trycloudflare.com/api'
+  apiUrl: 'https://politics-emily-polyester-recipient.trycloudflare.com/api'
 };
