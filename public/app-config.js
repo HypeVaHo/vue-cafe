ecef1db604944063efc53446229ca813965280b0
HypeVaHo <kxss8531@gmail.com>
2026-09-11 14:39:26 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://hip-montreal-patch-generic.trycloudflare.com/api'
+  apiUrl: 'https://grande-numerical-flashers-sophisticated.trycloudflare.com/api'
 };
