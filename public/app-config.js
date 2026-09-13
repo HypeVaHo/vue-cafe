c3283db6acc9f825359a35c000bcae272f586910
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:30:13 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://headphones-ends-webmaster-assure.trycloudflare.com/api'
+  apiUrl: 'https://identified-gtk-editing-composer.trycloudflare.com/api'
 };
