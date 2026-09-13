6aa3158256d73b867e09f30abd9a7beb6e0b2313
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:34:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://staying-democracy-satin-prot.trycloudflare.com/api'
+  apiUrl: 'https://utc-roll-browse-extends.trycloudflare.com/api'
 };
