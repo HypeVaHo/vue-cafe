9e673a7e215bfbf79bea65ed96ddf7be17720840
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:18:51 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://forecast-see-large-cooperative.trycloudflare.com/api'
+  apiUrl: 'https://charles-day-honolulu-seed.trycloudflare.com/api'
 };
