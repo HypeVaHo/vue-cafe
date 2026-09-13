67fb7300a0e3d17beb7650ad404a05fcb8f1791d
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:02:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://qui-sapphire-felt-demographic.trycloudflare.com/api'
+  apiUrl: 'https://grab-belong-not-cotton.trycloudflare.com/api'
 };
