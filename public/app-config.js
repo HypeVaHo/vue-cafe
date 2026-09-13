1e06f6533cf726de7513cd6f27711e18c969b7c4
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:16:09 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://minority-encourages-refer-toilet.trycloudflare.com/api'
+  apiUrl: 'https://enhancement-brilliant-forum-winter.trycloudflare.com/api'
 };
