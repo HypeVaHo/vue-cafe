a697217a2f59a5abc22b06ac18a72d26fee183ef
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:28:53 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://circulation-hundreds-flip-interactions.trycloudflare.com/api'
+  apiUrl: 'https://headphones-ends-webmaster-assure.trycloudflare.com/api'
 };
