c9e904a706d3b242d4060f9c3c8a079b70f755fc
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:10:55 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://fireplace-waiting-designated-walker.trycloudflare.com/api'
+  apiUrl: 'https://quotations-induction-bookstore-attorneys.trycloudflare.com/api'
 };
