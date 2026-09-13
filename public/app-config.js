e6066eab920e981a0753378b0161b347c1fcad19
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:00:45 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://cast-sense-hundred-infectious.trycloudflare.com/api'
+  apiUrl: 'https://qui-sapphire-felt-demographic.trycloudflare.com/api'
 };
