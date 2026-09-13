e8121845f2ffbb16776f870071ef7d961dc72dac
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:09:36 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://adapters-ooo-pearl-meditation.trycloudflare.com/api'
+  apiUrl: 'https://fireplace-waiting-designated-walker.trycloudflare.com/api'
 };
