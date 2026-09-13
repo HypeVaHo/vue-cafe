c63caa6e71299f7191035fbf5a1650a21966a085
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:14:49 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://substitute-vocabulary-tribes-showers.trycloudflare.com/api'
+  apiUrl: 'https://minority-encourages-refer-toilet.trycloudflare.com/api'
 };
