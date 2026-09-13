b4f6a35ffdc61cbca6493bdbbf314fd9d249541c
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:42:54 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://briefing-tests-love-southeast.trycloudflare.com/api'
+  apiUrl: 'https://smooth-cases-push.loca.lt/api'
 };
