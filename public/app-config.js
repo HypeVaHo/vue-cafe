645b3451e0cd0293f0252570b38365588660f3c3
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:13:29 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://outcome-eur-sure-born.trycloudflare.com/api'
+  apiUrl: 'https://substitute-vocabulary-tribes-showers.trycloudflare.com/api'
 };
