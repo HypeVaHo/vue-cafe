e257ac3c4d9bef2b2a5476e99c477217df7dd685
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:38:00 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://entrance-mothers-occurs-crew.trycloudflare.com/api'
+  apiUrl: 'https://briefing-tests-love-southeast.trycloudflare.com/api'
 };
