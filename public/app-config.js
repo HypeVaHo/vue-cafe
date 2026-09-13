a302f8bcf9afa7c6c478c335d2e7bdfb27bd061b
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:12:14 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://quotations-induction-bookstore-attorneys.trycloudflare.com/api'
+  apiUrl: 'https://outcome-eur-sure-born.trycloudflare.com/api'
 };
