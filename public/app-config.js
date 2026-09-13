234a2eaf6404f96ea93a69358694831564de4a30
HypeVaHo <kxss8531@gmail.com>
2026-09-13 16:22:36 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://additional-retreat-featuring-coding.trycloudflare.com/api'
+  apiUrl: 'https://partial-them-kidney-jackie.trycloudflare.com/api'
 };
