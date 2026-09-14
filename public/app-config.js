9dbd92e302d64f988c4ff1af7d9e4f48ac8743ae
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:32:25 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://phantom-cow-phillips-sensitivity.trycloudflare.com/api'
+  apiUrl: 'https://experimental-often-cartridge-likelihood.trycloudflare.com/api'
 };
