b2bd21b8612fb16cf315f13d3711679370f1b9e0
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:31:05 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://casual-fragrances-overview-phillips.trycloudflare.com/api'
+  apiUrl: 'https://phantom-cow-phillips-sensitivity.trycloudflare.com/api'
 };
