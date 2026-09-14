76889ee5247b772a98716b5c624f64d3e707fd5d
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:24:47 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://priest-closest-gba-hope.trycloudflare.com/api'
+  apiUrl: 'https://buy-institutions-nominations-vision.trycloudflare.com/api'
 };
