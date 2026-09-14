76e9fb96a966292ecd8ab0c02d92d8859adeed7d
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:22:22 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://appeals-cir-shipping-communication.trycloudflare.com/api'
+  apiUrl: 'https://priest-closest-gba-hope.trycloudflare.com/api'
 };
