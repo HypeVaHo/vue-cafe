13f352688b8423be5076aba115f57df761443d6e
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:27:32 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://preceding-tongue-comments-trust.trycloudflare.com/api'
+  apiUrl: 'https://casual-fragrances-overview-phillips.trycloudflare.com/api'
 };
