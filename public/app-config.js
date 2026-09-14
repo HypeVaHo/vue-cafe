cd847f1544e57caae51634700cb73ccac9f65849
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:17:06 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://ppc-sbjct-headphones-claims.trycloudflare.com/api'
+  apiUrl: 'https://conflicts-black-minutes-processors.trycloudflare.com/api'
 };
