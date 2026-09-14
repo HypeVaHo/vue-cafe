3e19b9b96b041696ffdb3c3eafeae244827ee907
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:26:13 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://buy-institutions-nominations-vision.trycloudflare.com/api'
+  apiUrl: 'https://preceding-tongue-comments-trust.trycloudflare.com/api'
 };
