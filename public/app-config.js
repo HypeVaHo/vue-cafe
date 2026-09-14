106edcc204dd681ba3513e16d6bcac447b6b3f8c
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:37:51 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://managed-everyday-like-dont.trycloudflare.com/api'
+  apiUrl: 'https://grab-francisco-institutes-computation.trycloudflare.com/api'
 };
