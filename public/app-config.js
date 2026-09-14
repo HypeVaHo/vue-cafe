77833250270a781e54344571dd3fc8029d19e49c
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:11:35 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://perry-raid-sure-detector.trycloudflare.com/api'
+  apiUrl: 'https://answer-league-joseph-clusters.trycloudflare.com/api'
 };
