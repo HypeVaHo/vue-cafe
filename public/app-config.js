8d0d07b876a6037fe0be904a0955a512ed6b08a4
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:54:58 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://ant-goat-movie-fundamentals.trycloudflare.com/api'
+  apiUrl: 'https://polite-jobs-dress.loca.lt/api'
 };
