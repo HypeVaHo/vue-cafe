babb7a0804a4b54cdecce3af3f95db65e80f0d13
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:36:32 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://fuzzy-adaptive-delivers-walk.trycloudflare.com/api'
+  apiUrl: 'https://managed-everyday-like-dont.trycloudflare.com/api'
 };
