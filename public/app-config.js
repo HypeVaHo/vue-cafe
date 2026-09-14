21da74eeb68e214b12a7708d3591bf05b4e4a778
HypeVaHo <kxss8531@gmail.com>
2026-09-14 19:52:07 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://polite-jobs-dress.loca.lt/api'
+  apiUrl: 'https://reporters-charleston-scientists-bumper.trycloudflare.com/api'
 };
