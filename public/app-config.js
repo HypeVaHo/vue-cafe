0370d3ef45f66105a9e961f55aedf01f5c16dc6f
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:52:48 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://francisco-associate-grad-informative.trycloudflare.com/api'
+  apiUrl: 'https://ant-goat-movie-fundamentals.trycloudflare.com/api'
 };
