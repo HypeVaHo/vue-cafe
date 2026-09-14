5ff1938b8eddb97e712f0bfdcee8d6090479d3a8
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:18:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://conflicts-black-minutes-processors.trycloudflare.com/api'
+  apiUrl: 'https://stable-alone-continental-having.trycloudflare.com/api'
 };
