6ec682b153d0f24a7c8ab52704e96c3919a814ef
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:40:30 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://shaft-dealing-folding-armed.trycloudflare.com/api'
+  apiUrl: 'https://spencer-highs-legacy-arc.trycloudflare.com/api'
 };
