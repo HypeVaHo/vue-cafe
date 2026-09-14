aa63c89d8dc5c9831a6b1addcc243037f3f63968
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:50:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://disciplines-scanners-rules-among.trycloudflare.com/api'
+  apiUrl: 'https://francisco-associate-grad-informative.trycloudflare.com/api'
 };
