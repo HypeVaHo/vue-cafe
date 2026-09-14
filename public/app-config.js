7a58615bae7c68d971da85fa046ebd845567f07b
HypeVaHo <kxss8531@gmail.com>
2026-09-14 10:19:53 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://stable-alone-continental-having.trycloudflare.com/api'
+  apiUrl: 'https://appeals-cir-shipping-communication.trycloudflare.com/api'
 };
