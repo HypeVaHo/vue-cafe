e2cb57ec2db05f56f6447ed0dc3c58d25ad62fac
HypeVaHo <kxss8531@gmail.com>
2026-09-15 15:18:26 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://alex-remainder-dependent-taxi.trycloudflare.com/api'
+  apiUrl: 'https://optimal-rabbit-princeton-powder.trycloudflare.com/api'
 };
