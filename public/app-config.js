addee1e284c747f368f2b14b33c3d85cb2a3c859
HypeVaHo <kxss8531@gmail.com>
2026-09-15 15:34:28 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://petite-steaks-stop.loca.lt/api'
+  apiUrl: 'https://consisting-broader-miracle-elder.trycloudflare.com/api'
 };
