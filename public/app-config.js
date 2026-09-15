6cc5aa7ea0118ce49c522df23710950fb91982f7
HypeVaHo <kxss8531@gmail.com>
2026-09-15 15:13:45 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://button-pre-reproduced-essay.trycloudflare.com/api'
+  apiUrl: 'https://telephony-gathered-useful-prisoner.trycloudflare.com/api'
 };
