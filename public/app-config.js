dcfeba93a2a30d383b67779c66f792f36d302c33
HypeVaHo <kxss8531@gmail.com>
2026-09-15 15:12:25 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://shaw-vancouver-acm-saw.trycloudflare.com/api'
+  apiUrl: 'https://button-pre-reproduced-essay.trycloudflare.com/api'
 };
