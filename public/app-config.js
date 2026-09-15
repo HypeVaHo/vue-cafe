5f32200a9ed2596629af95b0547fe09d943f7019
HypeVaHo <kxss8531@gmail.com>
2026-09-15 15:19:47 +1000

    Update API tunnel URL [auto]

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,4 @@
 // Konfig: auto by server-guard.ps1
 window.APP_CONFIG = {
-  apiUrl: 'https://optimal-rabbit-princeton-powder.trycloudflare.com/api'
+  apiUrl: 'https://bernard-winter-millions-spelling.trycloudflare.com/api'
 };
